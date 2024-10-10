6c365e41082a3d801b5830c90ca740b065236b5c
Andik Yudo Setiawan <andikyudo85@gmail.com>
2024-10-10 23:21:28 +0700

    Ready to Deploy on Vercel III

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
-const i18nConfig = require("./next-i18next.config.js");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	i18n: i18nConfig.i18n,
 	reactStrictMode: true,
 	images: {
 		remotePatterns: [
