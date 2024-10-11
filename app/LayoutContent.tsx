291046564e07315a0b9cff54b19b03121414e88e
Andik Yudo Setiawan <andikyudo85@gmail.com>
2024-10-11 23:37:48 +0700

    Adding RunningAnnouncement Oke

--- app/LayoutContent.tsx
+++ app/LayoutContent.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React from "react";
-import Image from "next/image"; // Tambahkan import ini
-
+import Image from "next/image";
 import { useHeader } from "../components/HeaderContext";
 import ThemeToggle from "../components/ThemeToggle";
+import RunningAnnouncement from "../components/RunningAnnouncement";
 
 export default function LayoutContent({
 	children,
@@ -13,32 +13,37 @@ export default function LayoutContent({
 }) {
 	const { showHeader } = useHeader();
 
+	// You can replace this with actual announcement text, possibly from a context or prop
+	const announcementText =
+		"Welcome to Foreign Visitor Service. Please ensure all your documents are up to date.";
+
 	return (
-		<div className='min-h-screen flex flex-col bg-white from-blue-100 to-white dark:to-gray-900 '>
+		<div className='min-h-screen flex flex-col bg-white from-blue-100 to-white dark:to-gray-900'>
 			{showHeader && (
-				<header className='bg-white dark:bg-yellow-300 text-black p-6 shadow-xl'>
-					<div className='container mx-auto flex justify-between items-center'>
-						<div className='flex items-center'>
-							{" "}
-							{/* Wrapper untuk logo dan teks */}
-							<Image
-								src='/logo_polresta.png' // Ganti dengan path logo Anda
-								alt='Foreign Visitor Service Logo'
-								width={42} // Sesuaikan ukuran sesuai kebutuhan
-								height={42} // Sesuaikan ukuran sesuai kebutuhan
-								className='mr-2' // Margin kanan untuk memisahkan logo dari teks
-							/>
-							<div className='flex-wrap'>
-								<h1 className='text-sm font-bold'>Polresta Pontianak</h1>
-								<p className='text-sm'>Jln.Johan Idrus No.1 Pontianak</p>
-								<div className='border-b-2 border-cyan-950 w-full'></div>
+				<header className='bg-white dark:bg-yellow-300 text-black shadow-xl'>
+					<div className='container mx-auto p-6'>
+						<div className='flex justify-between items-center'>
+							<div className='flex items-center'>
+								<Image
+									src='/logo_polresta.png'
+									alt='Foreign Visitor Service Logo'
+									width={42}
+									height={42}
+									className='mr-2'
+								/>
+								<div className='flex-wrap'>
+									<h1 className='text-sm font-bold'>Polresta Pontianak</h1>
+									<p className='text-sm'>Jln.Johan Idrus No.1 Pontianak</p>
+									<div className='border-b-2 border-cyan-950 w-full'></div>
+								</div>
 							</div>
+							<ThemeToggle />
 						</div>
-						<ThemeToggle />
 					</div>
+					<RunningAnnouncement text={announcementText} />
 				</header>
 			)}
-			<main className='flex-grow flex items-center justify-center p-4'>
+			<main className='flex-grow flex items-center justify-center p-2'>
 				{children}
 			</main>
 		</div>
